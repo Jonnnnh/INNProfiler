02a0ca4a4a80b6e4342296cab6e52f551d429236
Daria <jonshilin.gg@gmail.com>
2024-04-16 01:18:10 +0300

    bugs fixed added more fragments

--- app.py
+++ app.py
@@ -34,23 +34,60 @@ def get_company_details(inn):
             company_url = "https://www.rusprofile.ru" + company_link['href']
             company_response = requests.get(company_url, headers=headers)
             company_soup = BeautifulSoup(company_response.text, 'html.parser')
+
             name = company_soup.find('div', class_='company-name').get_text(strip=True) if company_soup.find('div', class_='company-name') else "Название не найдено"
+            inn_kpp = company_soup.find('dd', class_='company-info__text has-copy', itemprop='taxID').get_text(strip=True) if company_soup.find('dd', class_='company-info__text has-copy', itemprop='taxID') else "ИНН/КПП не найдено"
+
+            authorized_capital = None
+            for dt in company_soup.find_all('dt', class_='company-info__title'):
+                if dt.get_text(strip=True) == 'Уставный капитал':
+                    dd = dt.find_next_sibling('dd')
+                    if dd:
+                        authorized_capital = dd.get_text(strip=True)
+            authorized_capital = authorized_capital or "Уставной капитал не найден"
+
+            leader_info_block = company_soup.find('div', class_='company-row hidden-parent')
+            if leader_info_block:
+                leader_name_tag = leader_info_block.find('a', class_='gtm_main_fl')
+                leader_name = leader_name_tag.get_text(strip=True) if leader_name_tag else "Руководитель не найдено"
+            else:
+                leader_name = "Руководитель не найдено"
+
+            legal_address = company_soup.find('address', itemprop='address').get_text(strip=True) if company_soup.find(
+                'address', itemprop='address') else "Юридический адрес не найдено"
 
         else:
             title_tag = soup.find('title')
             name = title_tag.get_text().split('(')[0].strip() if title_tag else "Название не найдено"
-            inn = soup.find('span', id="clip_inn").get_text(strip=True) if soup.find('span', id="clip_inn") else "ИНН не найдено"
+            inn_kpp = soup.find('dd', class_='company-info__text has-copy', itemprop='taxID').get_text(strip=True) if soup.find('dd', class_='company-info__text has-copy', itemprop='taxID') else "ИНН/КПП не найдено"
+
+            authorized_capital = None
+            for dt in soup.find_all('dt', class_='company-info__title'):
+                if dt.get_text(strip=True) == 'Уставный капитал':
+                    dd = dt.find_next_sibling('dd')
+                    if dd:
+                        authorized_capital = dd.get_text(strip=True)
+            authorized_capital = authorized_capital or "Уставной капитал не найден"
+
             ogrn = soup.find('span', id="clip_ogrn").get_text(strip=True) if soup.find('span', id="clip_ogrn") else "ОГРН не найдено"
             registration_date = soup.find('dd', itemprop='foundingDate').get_text( strip=True) if soup.find('dd', itemprop='foundingDate') else "Дата регистрации не найдено"
-            leader = soup.find('div', {'class': 'chief'}).get_text(strip=True) if soup.find('div', {'class': 'chief'}) else "Руководитель не найдено"
-            legal_address = soup.find('address', itemprop='address').get_text(strip=True) if soup.find('span', itemprop='address') else "Юридический адрес не найдено"
 
+            leader_info_block = soup.find('div', class_='company-row hidden-parent')
+            if leader_info_block:
+                leader_name_tag = leader_info_block.find('a', class_='gtm_main_fl')
+                leader_name = leader_name_tag.get_text(strip=True) if leader_name_tag else "Руководитель не найдено"
+            else:
+                leader_name = "Руководитель не найдено"
+
+            legal_address = soup.find('address', itemprop='address').get_text(strip=True) if soup.find(
+                'address', itemprop='address') else "Юридический адрес не найдено"
         return {
             'company_name': name,
-            'inn': inn,
+            'inn_kpp': inn_kpp,
+            'authorized_capital': authorized_capital,
             'ogrn': ogrn,
             'registration_date': registration_date,
-            'leader': leader,
+            'leader_name': leader_name,
             'legal_address': legal_address,
         }
     else:
